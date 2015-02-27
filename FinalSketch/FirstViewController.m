fd82c9528e3c201b6eb7ec789911901b62fb888a
xiaoyifan <xiaoyifan@pidgey.cs.uchicago.edu>
2015-02-26 22:01:57 -0600

    basic done

--- FinalSketch/FirstViewController.m
+++ FinalSketch/FirstViewController.m
@@ -10,12 +10,18 @@
 
 @interface FirstViewController ()
 
+@property (weak, nonatomic) IBOutlet UITableView *mainTableView;
+
 @end
 
 @implementation FirstViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.mainTableView.delegate = self;
+    self.mainTableView.dataSource = self;
+    self.view.backgroundColor = [UIColor colorWithRed:255/255.0 green:255/255.0 blue:240/255.0 alpha:1];
+    self.mainTableView.backgroundColor = [UIColor colorWithRed:255/255.0 green:255/255.0 blue:240/255.0 alpha:1];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +29,37 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+//    cell.contentView.backgroundColor = [UIColor colorWithRed:255/255.0 green:255/255.0 blue:220/255.0 alpha:1];
+    NSLog(@"%ld", (long)indexPath.row);
+    
+        UIImageView *av = [[UIImageView alloc] initWithFrame:CGRectMake(20, 20, 277, 58)];
+        av.backgroundColor = [UIColor clearColor];
+        av.opaque = NO;
+        av.image = [UIImage imageNamed:@"Chicago.jpg"];
+        cell.backgroundView = av;
+        cell.selectedBackgroundView = av;
+
+
+    
+    cell.textLabel.text = @"Hello table";
+    
+    return cell;
+}
+
+
 
 @end
